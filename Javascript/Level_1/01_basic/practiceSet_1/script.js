5b6bed19d3ca2438e0e1feb555c5695fc8bd1c76
Yash Pokhriyal <er.yash.pokhriyal@gmail.com>
2026-05-11 16:34:21 +0530

    Add files via upload

---
+++ Javascript/Level_1/01_basic/practiceSet_1/script.js
@@ -0,0 +1,21 @@
+// 1. Use an alert to display Good
+// Morning.
+// 2.And  Display your name in a popup.
+// 3. Using Math calculate the
+// following:
+// => 75-25
+// => 3+3-5
+// 4. Change your page to
+// display “I am Learning JS in console”
+
+// 1. 2.
+alert('Good Morning Dear Yash');
+
+// 3. 
+console.log(75-25);
+
+console.log(3+3-5);
+
+// 4.
+document.body.innerHTML = '<b>I am Learning JS</b>'
+
---
+++ Javascript/Level_1/01_basic/script.js
@@ -0,0 +1,16 @@
+// Logging in console 
+console.log('Hello javascript world');
+
+// To give alert 
+alert('This website is not safe .');
+
+//  To give warning
+console.warn('Hie i am here to warn you .');
+
+// To give error
+console.error('Hello i am error .');
+
+// To clear the console 
+console.clear()
+
+
